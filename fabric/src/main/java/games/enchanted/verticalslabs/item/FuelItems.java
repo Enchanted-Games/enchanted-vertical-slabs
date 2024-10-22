1c26d08f6644c88d753926feb54e6b3a13184f54
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-10-22 22:02:29 +0100

    fix fabric fuels

--- fabric/src/main/java/games/enchanted/verticalslabs/item/FuelItems.java
+++ fabric/src/main/java/games/enchanted/verticalslabs/item/FuelItems.java
@@ -1,20 +1,22 @@
 package games.enchanted.verticalslabs.item;
 
 import games.enchanted.verticalslabs.block.ModBlocks;
-import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 
 public class FuelItems {
     public static void registerFuelItems() {
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_OAK_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_SPRUCE_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_BIRCH_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_JUNGLE_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_ACACIA_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_DARK_OAK_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_MANGROVE_SLAB.getBlock().orElseThrow(), 300);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_CHERRY_SLAB.getBlock().orElseThrow(), 300);
+        FuelRegistryEvents.BUILD.register((builder, context) -> {
+            builder.add(ModBlocks.VERTICAL_OAK_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_SPRUCE_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_BIRCH_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_JUNGLE_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_ACACIA_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_DARK_OAK_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_MANGROVE_SLAB.getBlock().orElseThrow(), 300);
+            builder.add(ModBlocks.VERTICAL_CHERRY_SLAB.getBlock().orElseThrow(), 300);
 
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_BAMBOO_SLAB.getBlock().orElseThrow(), 150);
-        FuelRegistry.INSTANCE.add(ModBlocks.VERTICAL_BAMBOO_MOSAIC_SLAB.getBlock().orElseThrow(), 150);
+            builder.add(ModBlocks.VERTICAL_BAMBOO_SLAB.getBlock().orElseThrow(), 150);
+            builder.add(ModBlocks.VERTICAL_BAMBOO_MOSAIC_SLAB.getBlock().orElseThrow(), 150);
+        });
     }
 }
